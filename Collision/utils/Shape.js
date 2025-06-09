64449538a828bdd6f27aa4a1beffcb7cda118c79
Gani Galiyev <Gani.Galiyev@ventionteams.com>
2025-06-09 18:11:31 +0500

    add demo collision effect

--- Collision/utils/Shape.js
+++ Collision/utils/Shape.js
@@ -1,3 +1,6 @@
+import isColliding from "./isColliding.js";
+import CheckWhichSideIsColliding from "./isColliding.js";
+
 export default class Shape {
   static shapesList = [];
   constructor() {
@@ -13,10 +16,10 @@ export default class Shape {
 
   isOverlap(shape1, shape2) {
     return !(
-      shape1.right <= shape2.left ||
-      shape1.left >= shape2.right ||
-      shape1.bottom <= shape2.top ||
-      shape1.top >= shape2.bottom
+      shape1.right + 20 <= shape2.left ||
+      shape1.left - 20 >= shape2.right ||
+      shape1.bottom + 20 <= shape2.top ||
+      shape1.top - 20 >= shape2.bottom
     );
   }
 
@@ -65,36 +68,11 @@ export default class Shape {
         targetShape.style.top = zone.height - targetShape.height - 1 + "px";
       }
     }
-    this.isColliding(targetShape);
+    isColliding(targetShape);
   }
 
   dropShape() {
     document.removeEventListener("mousemove", this.boundMoveShape);
     document.removeEventListener("mouseup", this.boundDropShape);
-  }
-
-  isColliding(targetShape) {
-    let targetShapeСoordinates = targetShape.getBoundingClientRect();
-
-    for (let shape of Shape.shapesList) {
-      let currentShapeСoordinates = shape.getBoundingClientRect();
-
-      if (
-        ((targetShapeСoordinates.left > currentShapeСoordinates.left &&
-          targetShapeСoordinates.left - 15 < currentShapeСoordinates.right) ||
-          (targetShapeСoordinates.right + 15 > currentShapeСoordinates.left &&
-            targetShapeСoordinates.right < currentShapeСoordinates.right)) &&
-        ((targetShapeСoordinates.top > currentShapeСoordinates.top &&
-          targetShapeСoordinates.top - 15 < currentShapeСoordinates.bottom) ||
-          (targetShapeСoordinates.bottom + 15 > currentShapeСoordinates.top &&
-            targetShapeСoordinates.bottom < currentShapeСoordinates.bottom))
-      ) {
-        targetShape.style.border = "1px solid red";
-        shape.style.border = "1px solid red";
-      } else {
-        targetShape.style.border = "1px solid white";
-        shape.style.border = "1px solid white";
-      }
-    }
   }
 }
---
+++ Collision/utils/isColliding.js
@@ -0,0 +1,136 @@
+import Shape from "./Shape.js";
+
+export default function isColliding(targetShape) {
+  let targetShapeСoordinates = targetShape.getBoundingClientRect();
+
+  for (let shape of Shape.shapesList) {
+    let currentShapeСoordinates = shape.getBoundingClientRect();
+
+    if (
+      ((targetShapeСoordinates.left > currentShapeСoordinates.left &&
+        targetShapeСoordinates.left - 15 < currentShapeСoordinates.right) ||
+        (targetShapeСoordinates.right + 15 > currentShapeСoordinates.left &&
+          targetShapeСoordinates.right < currentShapeСoordinates.right)) &&
+      ((targetShapeСoordinates.top > currentShapeСoordinates.top &&
+        targetShapeСoordinates.top - 15 < currentShapeСoordinates.bottom) ||
+        (targetShapeСoordinates.bottom + 15 > currentShapeСoordinates.top &&
+          targetShapeСoordinates.bottom < currentShapeСoordinates.bottom))
+    ) {
+      checkCollisionSide(targetShape, shape);
+    } else {
+      targetShape.style.border = "1px solid white";
+      shape.style.border = "1px solid white";
+    }
+  }
+}
+
+function checkCollisionSide(shape1, shape2) {
+  let shape1Сoordinates = shape1.getBoundingClientRect();
+  let shape2Сoordinates = shape2.getBoundingClientRect();
+
+  if (
+    shape1Сoordinates.right > shape2Сoordinates.left &&
+    shape1Сoordinates.right < shape2Сoordinates.right &&
+    shape1Сoordinates.bottom + 15 > shape2Сoordinates.top &&
+    shape1Сoordinates.bottom < shape2Сoordinates.bottom
+  ) {
+    shape1.style.borderBottom = "4px solid red";
+    shape2.style.borderTop = "4px solid green";
+
+    shape1.style.top =
+      Math.round(shape2.offsetTop - shape2Сoordinates.height) + "px";
+
+    shape1.style.left = Math.round(shape2.offsetLeft) + "px"; // Cделано
+  } else if (
+    shape1Сoordinates.right + 15 > shape2Сoordinates.left &&
+    shape1Сoordinates.right < shape2Сoordinates.right &&
+    shape1Сoordinates.bottom > shape2Сoordinates.top &&
+    shape1Сoordinates.bottom < shape2Сoordinates.bottom
+  ) {
+    shape1.style.borderRight = "4px solid red";
+    shape2.style.borderLeft = "4px solid green";
+
+    shape1.style.left =
+      Math.round(shape2.offsetLeft - shape2Сoordinates.width) + "px";
+    shape1.style.top = Math.round(shape2.offsetTop) + "px"; // Cделано
+  } else if (
+    shape1Сoordinates.right + 15 > shape2Сoordinates.left &&
+    shape1Сoordinates.right < shape2Сoordinates.right &&
+    shape1Сoordinates.bottom + 15 > shape2Сoordinates.top &&
+    shape1Сoordinates.bottom < shape2Сoordinates.bottom
+  ) {
+    shape1.style.borderRight = "4px solid red";
+    shape1.style.borderBottom = "4px solid red";
+    shape2.style.borderLeft = "4px solid red";
+    shape2.style.borderTop = "4px solid red"; // Позже
+  } else if (
+    shape1Сoordinates.left > shape2Сoordinates.left &&
+    shape1Сoordinates.left - 15 < shape2Сoordinates.right &&
+    shape1Сoordinates.bottom > shape2Сoordinates.top &&
+    shape1Сoordinates.bottom < shape2Сoordinates.bottom
+  ) {
+    shape1.style.borderLeft = "4px solid red";
+    shape2.style.borderRight = "4px solid green";
+
+    shape1.style.left =
+      Math.round(shape2.offsetLeft + shape2Сoordinates.width) + "px";
+    shape1.style.top = Math.round(shape2.offsetTop) + "px"; //Cделано
+  } else if (
+    shape1Сoordinates.left > shape2Сoordinates.left &&
+    shape1Сoordinates.left < shape2Сoordinates.right &&
+    shape1Сoordinates.top > shape2Сoordinates.top &&
+    shape1Сoordinates.top - 15 < shape2Сoordinates.bottom
+  ) {
+    shape1.style.borderTop = "4px solid red";
+    shape2.style.borderBottom = "4px solid green";
+
+    shape1.style.top =
+      Math.round(shape2.offsetTop + shape2Сoordinates.height) + "px";
+    shape1.style.left = Math.round(shape2.offsetLeft) + "px"; //Сделано
+  } else if (
+    shape1Сoordinates.left > shape2Сoordinates.left &&
+    shape1Сoordinates.left - 15 < shape2Сoordinates.right &&
+    shape1Сoordinates.bottom + 15 > shape2Сoordinates.top &&
+    shape1Сoordinates.bottom < shape2Сoordinates.bottom
+  ) {
+    shape1.style.borderLeft = "4px solid red";
+    shape1.style.borderBottom = "4px solid red";
+    shape2.style.borderRight = "4px solid red";
+    shape2.style.borderTop = "4px solid red"; // Позже
+  } else if (
+    shape1Сoordinates.right + 15 > shape2Сoordinates.left &&
+    shape1Сoordinates.right < shape2Сoordinates.right &&
+    shape1Сoordinates.top > shape2Сoordinates.top &&
+    shape1Сoordinates.top < shape2Сoordinates.bottom
+  ) {
+    shape1.style.borderRight = "4px solid red";
+    shape2.style.borderLeft = "4px solid green";
+
+    shape1.style.left =
+      Math.round(shape2.offsetLeft - shape2Сoordinates.width) + "px";
+    shape1.style.top = Math.round(shape2.offsetTop) + "px"; //Cделано
+  } else if (
+    shape1Сoordinates.right > shape2Сoordinates.left &&
+    shape1Сoordinates.right < shape2Сoordinates.right &&
+    shape1Сoordinates.top > shape2Сoordinates.top &&
+    shape1Сoordinates.top - 15 < shape2Сoordinates.bottom
+  ) {
+    shape1.style.borderTop = "4px solid red";
+    shape2.style.borderBottom = "4px solid green";
+  } else if (
+    shape1Сoordinates.right + 15 > shape2Сoordinates.left &&
+    shape1Сoordinates.right < shape2Сoordinates.right &&
+    shape1Сoordinates.top > shape2Сoordinates.top &&
+    shape1Сoordinates.top - 15 < shape2Сoordinates.bottom
+  ) {
+    shape1.style.borderRight = "4px solid red";
+    shape1.style.borderTop = "4px solid red";
+    shape2.style.borderLeft = "4px solid red";
+    shape2.style.borderBottom = "4px solid red";
+  } else {
+    shape1.style.borderLeft = "4px solid red";
+    shape1.style.borderTop = "4px solid red";
+    shape2.style.borderRight = "4px solid red";
+    shape2.style.borderBottom = "4px solid red";
+  }
+}
